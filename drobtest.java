f028c13d2bebea3d10ceb280ccb105fa065fa80d
Liagushka123 <155446168+Liagushka123@users.noreply.github.com>
2024-01-02 15:18:15 +0300

    Add files via upload

---
+++ drobtest.java
@@ -0,0 +1,10 @@
+﻿public class drobtest {
+    
+    public static void main(String[] args){
+    drob dsd = new drob(1, 4, 7);
+    System.out.println(dsd);
+    dsd.writtt();
+    dsd.nepravilnaia();
+}
+    
+}
