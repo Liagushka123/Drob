2fc4c02400a4f1858c150ab4ed624958f2f42c3f
Mikhail Zhdanyuk <zhdanyuk.mikhail@gmail.com>
2024-01-02 22:56:48 +0300

    правка отступов

--- drobtest.java
+++ drobtest.java
@@ -1,20 +1,20 @@
 ﻿public class drobtest {
     
     public static void main(String[] args){
-    drob test = new drob(2, 16, 3);
-    drob dsd = new drob(1, 4, 7);
-    
-    System.out.println("dsd: " + dsd);
-    dsd.pravelnoe();
-    System.out.println("dsd: " + dsd);
-    dsd.nepravilnaia();
-    System.out.println("dsd: " + dsd);
+        drob test = new drob(2, 16, 3);
+        drob dsd = new drob(1, 4, 7);
+        
+        System.out.println("dsd: " + dsd);
+        dsd.pravelnoe();
+        System.out.println("dsd: " + dsd);
+        dsd.nepravilnaia();
+        System.out.println("dsd: " + dsd);
 
-    System.out.println("test: " + test);
-    test.pravelnoe();
-    System.out.println("test: " + test);
-    test.nepravilnaia();
-    System.out.println("test: " + test );   
-}
+        System.out.println("test: " + test);
+        test.pravelnoe();
+        System.out.println("test: " + test);
+        test.nepravilnaia();
+        System.out.println("test: " + test );   
+    }
     
 }
