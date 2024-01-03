a24ee50d6eb429f4739035108f2e6caf9aac9062
Mikhail Zhdanyuk <zhdanyuk.mikhail@gmail.com>
2024-01-03 20:46:18 +0300

    Добавлены операции и сокращение.

--- drob.java
+++ drob.java
@@ -19,10 +19,42 @@ public class drob {
         b = a * c + b;
         a = 0;
     }
-    public void slojenie(){
-         
+    public drob slojenie(drob nameDrob){
+        return new drob(a + nameDrob.a, b * nameDrob.c + nameDrob.b * c, c * nameDrob.c);
     }
     
+    public drob vichitanie(drob lDrob){
+        return new drob(a - lDrob.a,b * lDrob.c - lDrob.b * c, c * lDrob.c);
+    }
+    
+    public drob ymnojenie(drob Xdrob){
+        Xdrob.nepravilnaia();
+        this.nepravilnaia();
+        return new drob(a * Xdrob.a,b * Xdrob.b,c * Xdrob.c);
+    }
+    public drob dilenie(drob Mdrob){
+        this.nepravilnaia();
+        Mdrob.nepravilnaia();
+        return new drob(a * Mdrob.a,b * Mdrob.c, c * Mdrob.b );
+    }
+    public void socrashenie(){
+        int h = 2;
+        while(h <= b ){
+            if (b % h == 0 && c % h == 0) {
+                System.out.println("делитель: "+h );
+                b = b / h;
+                c = c / h;
+            }
+            else{
+                h = h+1;
+            
+            }
+        }
+
+    }
+
+
+
     @Override
     public String toString() {
         return "Drob: " + a +"  " + b +"  " + c;
--- drobtest.java
+++ drobtest.java
@@ -3,18 +3,34 @@
     public static void main(String[] args){
         drob test = new drob(2, 16, 3);
         drob dsd = new drob(1, 4, 7);
-        
-        System.out.println("dsd: " + dsd);
-        dsd.pravelnoe();
-        System.out.println("dsd: " + dsd);
-        dsd.nepravilnaia();
-        System.out.println("dsd: " + dsd);
+        drob test1 = new drob(0, 8, 12);
+        // System.out.println("dsd: " + dsd);
+        // dsd.pravelnoe();
+        // System.out.println("dsd: " + dsd);
+        // dsd.nepravilnaia();
+        // System.out.println("dsd: " + dsd);
 
-        System.out.println("test: " + test);
-        test.pravelnoe();
-        System.out.println("test: " + test);
-        test.nepravilnaia();
-        System.out.println("test: " + test );   
+        // System.out.println("test: " + test);
+        // test.pravelnoe();
+        // System.out.println("test: " + test);
+        // test.nepravilnaia();
+        // System.out.println("test: " + test );   
+        
+        drob ddrob =  test.slojenie(dsd);
+        System.out.println(ddrob);
+        drob gDrob = test.vichitanie(dsd);
+        System.out.println(gDrob);
+        drob zDrob = test.ymnojenie(dsd);
+        System.out.println(zDrob);
+        drob pDrob = test.dilenie(dsd);
+        System.out.println(pDrob);
+        dsd.socrashenie();
+        System.out.println(dsd);
+        pDrob.socrashenie();
+        System.out.println(pDrob);
+        test1.socrashenie();
+        System.out.println(test1);
+
     }
     
 }
